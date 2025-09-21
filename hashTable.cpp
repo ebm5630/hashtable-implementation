362568284cf4d747cc4a1e111d03c437005619ca
ebm5630 <ebm5630@psu.edu>
2025-09-21 17:32:24 -0400

    std::unordered_maps

--- hashTable.cpp
+++ hashTable.cpp
@@ -1,142 +1,111 @@
-#include <iostream>
+#include <vector>
+#include <list>
+#include <utility>
 #include <string>
+#include <iostream>
+//  figure out how std hash works
+// how many buckets when u construct the hash table
+// no memory leakage
 
-// size of the hash table
-const int TABLE_SIZE = 4;
+template <typename K, typename V, typename Hash = std::hash<K>>
 
-  class HashTable
+class HashMap
+{
+private:
+  std::vector<std::list<std::pair<K, V>>> table;
+  size_t bucket_count;
+  Hash hasher;
+
+  size_t get_index(const K &key) const
   {
-  private:
+    return hasher(key) % bucket_count;
+  }
 
-    // this will the linked list node
-    struct Node
+  size_t get_bucket_number() const
+  {
+    return bucket_count;
+  }
+
+public:
+  HashMap(size_t buckets, Hash h = Hash()) : bucket_count(buckets), table(buckets), hasher(h) {
+    if (buckets == 0) {
+        throw std::invalid_argument("Number of buckets must be greater than 0");
+    }
+    else {
+        bucket_count = buckets;
+    }
+    table.resize(bucket_count);
+  }
+
+  // if 1 is returned we added a new {key, value}
+  // if 0 is returned we update the value
+  int insert(const K &key, const V &value)  
+  {
+    size_t index = get_index(key);
+    auto &bucket = table[index];
+    for (auto &pair : bucket)
     {
-      int key;
-      std::string value;
-      Node *next;
-
-      Node(int k, std::string v) : key(k), value(v), next(nullptr) {}
-    };
-    Node *table[TABLE_SIZE]; //this is basically the head of the linked list 
-
-    public:
-
-    // constructor nulls out the table
-    HashTable(){
-      for (int i = 0; i < TABLE_SIZE; i++)
-        table[i] = nullptr;
-    }
-    
-    int  hashFunction(int key){
-      int a = 3;
-      int b = 1;
-      int value = (a * key + b);
-      return value % TABLE_SIZE;
-    } 
-
-    void insert(int key, const std::string& value){
-      int index = hashFunction(key);
-      Node* newNode = new Node(key, value);
-
-      // in the case the bucket is empty
-      if(table[index] == nullptr){
-        table[index] = newNode;
-      } else {
-        // collision, we need to add to the linked list 
-        Node* current = table[index];
-        while(current->next != nullptr){
-          current = current->next;
-        }
-        current->next = newNode;
+      // since we cannot have the same key twice, we chekcing if the key already exists and updating the value
+      if (pair.first == key)
+      {
+        pair.second = value;
+        return 0;
       }
     }
-
-    void find(int key){
-      int index = hashFunction(key);
-      Node* current = table[index];
-      // checking the linked list for the key 
-      while(current != nullptr){
-        if(current->key == key){
-          std::cout << "Found: " << current->value << std::endl;
-          return;
-        }
-        current = current->next;
-      }
-      std::cout << "Key not found" << std::endl;
-    }
-
-    void remove(int key){
-      int index = hashFunction(key);
-      Node* current = table[index];
-      Node* prev = nullptr;
-
-      while(current != nullptr){
-        if(current->key == key){
-          if(prev == nullptr){
-            // removing the head of the list
-            table[index] = current->next;
-          } else {
-            prev->next = current->next;
-          }
-          delete current;
-          std::cout << "Key removed" << std::endl;
-          return;
-        }
-        prev = current;
-        current = current->next;
-      }
-      std::cout << "Key not found" << std::endl;
-    }
-
-    void erase(int key, const std::string& value){
-      int index = hashFunction(key);
-      Node* current = table[index];
-      Node* prev = nullptr;
-
-      while(current != nullptr){
-        if(current->key == key && current->value == value){
-          if(prev == nullptr){
-            // removing the head of the list
-            table[index] = current->next;
-          } else {
-            prev->next = current->next;
-          }
-          delete current;
-          std::cout << "Key-Value pair removed" << std::endl;
-          return;
-        }
-        prev = current;
-        current = current->next;
-      }
-      std::cout << "Key-Value pair not found" << std::endl;
-    }
-
+    // this would add the element at the end of the bucket
+    bucket.emplace_back(key, value);
+    return 1;
   };
 
+  // const so they don't change anything
+  const V *find(const K &key) const 
+  {
+    size_t index = get_index(key);
+    auto &bucket = table[index];
+    for (auto &pair : bucket)
+    {
+      if (pair.first == key)
+      {
+        return &pair.second;
+      }
+    }
+    return nullptr;
+  };
 
+  // 1 -> success, 0 -> key not found
+  size_t  erase(const K &key) 
+  {
+    size_t index = get_index(key);
+    auto &bucket = table[index];
+    for (auto it = bucket.begin(); it != bucket.end(); ++it)
+    {
+      if (it->first == key)
+      {
+        bucket.erase(it);
+        return 1;
+      }
+    }
+    return 0;
+  };
+
+  // this is redundant since key is always unique
+  size_t  erase(const K &key, const V &value)
+  {
+    size_t index = get_index(key);
+    auto &bucket = table[index];
+    for (auto it = bucket.begin(); it != bucket.end(); ++it)
+    {
+      if (it->first == key && it->second == value)
+      {
+        bucket.erase(it);
+        return 1;
+      }
+    }
+    return 0;
+  };
+};
 
 
 int main() {
-    HashTable ht;
-
-    // Insert some values
-    ht.insert(1, "Alice");
-    ht.insert(5, "Bob");       // will collide with 1 if TABLE_SIZE = 4
-    ht.insert(9, "Charlie");   // another collision
-
-    // Find values
-    ht.find(1);   // should find Alice
-    ht.find(5);   // should find Bob
-    ht.find(9);   // should find Charlie
-    ht.find(2);   // not found
-
-    // Remove values
-    ht.remove(5); // removes Bob
-    ht.find(5);   // should say "Key not found"
-
-    // Erase by key+value
-    ht.erase(1, "Alice");  // should remove Alice
-    ht.find(1);            // should say "Key not found"
-
-    return 0;
+  return 0;
 }
